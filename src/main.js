bc5bb6e5204d0dd616ac19081fb571e330676151
Guillaume Duhan <myhappyagency@gmail.com>
2020-01-28 20:49:54 +0100

    import firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import * as firebase from 'firebase/app'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -8,6 +9,18 @@ import MyPlugin from './plugins/my-plugin'
 
 Vue.use(BootstrapVue)
 Vue.use(MyPlugin)
+
+const configOptions = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESS_SENDER_ID,
+  appId: process.env.APP_ID,
+}
+
+firebase.initializeApp(configOptions)
 
 Vue.config.productionTip = false
 Vue.prototype.$log = console.log
