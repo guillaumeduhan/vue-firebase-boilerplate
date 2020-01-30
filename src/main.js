152a5a507e894c32165da3e23538eb4a7b89c1fb
Guillaume Duhan <myhappyagency@gmail.com>
2020-01-30 18:03:06 +0100

    update package.json & main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
-import * as firebase from 'firebase/app'
 import App from './App.vue'
 import router from './router'
 import store from './store'
